916c3c047990d278102dd8f17e2bc71a5ce7249a
Henry Li <li_zhao_bin@hotmail.com>
2016-06-29 17:22:39 -0600

    Update MainActivity.java

--- app/src/main/java/com/example/hsport/catalog/MainActivity.java
+++ app/src/main/java/com/example/hsport/catalog/MainActivity.java
@@ -27,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
                 // test
                 // testing vcs push
                 // test-branch
-                // master-edits
+                // master-edit
             }
         });
     }
