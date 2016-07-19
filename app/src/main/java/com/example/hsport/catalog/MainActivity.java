fc435a83e66073a36355f37d2ec04c3fdbbd817e
bitscuit <li_zhao_bin@hotmail.com>
2016-07-19 17:46:37 -0600

    rebase comment 3

--- app/src/main/java/com/example/hsport/catalog/MainActivity.java
+++ app/src/main/java/com/example/hsport/catalog/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
         // initial commit 2
         // test rebase comment
         // test rebase comment 2
+        // test rebase comment 3
     }
 
     @Override
